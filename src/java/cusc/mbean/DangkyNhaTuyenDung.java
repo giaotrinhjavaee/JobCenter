5589ff408912b71935c065e5c8387bd09ebc14ca
Le Thi Minh Loan <Le Thi Minh Loan@minhloan>
2016-04-01 17:51:17 +0700

    01

--- src/java/cusc/mbean/DangkyNhaTuyenDung.java
+++ src/java/cusc/mbean/DangkyNhaTuyenDung.java
@@ -16,8 +16,54 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class DangkyNhaTuyenDung {
-    String email;
+    private String email;
+    private String matkhau;
+    private String tencongty;
+    private String motacongty;
+    private String websitecongty;
+    
     public DangkyNhaTuyenDung() {
+        
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+
+    public String getTencongty() {
+        return tencongty;
+    }
+
+    public void setTencongty(String tencongty) {
+        this.tencongty = tencongty;
+    }
+
+    public String getMotacongty() {
+        return motacongty;
+    }
+
+    public void setMotacongty(String motacongty) {
+        this.motacongty = motacongty;
+    }
+
+    public String getWebsitecongty() {
+        return websitecongty;
+    }
+
+    public void setWebsitecongty(String websitecongty) {
+        this.websitecongty = websitecongty;
     }
     
 }
