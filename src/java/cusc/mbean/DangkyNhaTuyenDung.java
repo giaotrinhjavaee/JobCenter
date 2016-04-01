c7d9377acd4ead15287e231bd5bc3c1309a9624f
Le Thi Minh Loan <Le Thi Minh Loan@minhloan>
2016-04-01 17:46:34 +0700

    01

---
+++ src/java/cusc/mbean/DangkyNhaTuyenDung.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cusc.mbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Le Thi Minh Loan
+ */
+@ManagedBean
+@RequestScoped
+public class DangkyNhaTuyenDung {
+
+    /**
+     * Creates a new instance of DangkyNhaTuyenDung
+     */
+    public DangkyNhaTuyenDung() {
+    }
+    
+}
